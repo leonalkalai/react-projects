d2031d198548d431fcddf3b148b8e533e07a017a
leonalkalai <koundouras@gmail.com>
2025-01-01 22:44:11 +0200

    edit Project fix

--- user/src/components/ProjectList.jsx
+++ user/src/components/ProjectList.jsx
@@ -96,7 +96,7 @@ const Project = (props) => {
 // Project list logic start
 
 const localhostPath = "http://localhost:5050"; // select this for localhost
-const netlifyPath = "https://brilliant-strudel-b8b3ca.netlify.app/api"; // select this for netlify
+const netlifyPath = "https://brilliant-strudel-b8b3ca.netlify.app/projects"; // select this for netlify
 const URL = netlifyPath; // select the netlify custom API
 
 export default function ProjectList() {
