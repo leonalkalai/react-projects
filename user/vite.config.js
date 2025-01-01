e817a8d256911f6c2fb1e2901d5c031ee7e329d5
leonalkalai <koundouras@gmail.com>
2025-01-01 23:11:44 +0200

    edit Project fix

--- user/vite.config.js
+++ user/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-projects/projects", // Set the base path for GitHub Pages
+  base: "/react-projects/projects/", // Set the base path for GitHub Pages
   plugins: [react()],
 });
