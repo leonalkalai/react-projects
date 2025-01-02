83145fb5e18c04e0960026ff29276981e0324d72
leonalkalai <koundouras@gmail.com>
2025-01-02 11:26:14 +0200

    fixed routes

--- user/src/components/Project.jsx
+++ user/src/components/Project.jsx
@@ -28,10 +28,10 @@ export default function Project() {
   const navigate = useNavigate(); // get url navigation history
 
   const localhostPath = "http://localhost:5050"; // select this for localhost
-  const netlifyPath = "https://brilliant-strudel-b8b3ca.netlify.app/api"; // select this for netlify
+  const netlifyPath = "https://brilliant-strudel-b8b3ca.netlify.app/projects"; // select this for netlify
   const URL = netlifyPath; // select the netlify custom API
 
-  const homePath = "/react-projects"; // select this if hosted on github pages
+  const homePath = "/react-projects/projects"; // select this if hosted on github pages
 
   // start useEffect
   useEffect(() => {
--- user/vite.config.js
+++ user/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-projects/", // Set the base path for GitHub Pages
+  base: "/react-projects/projects", // Set the base path for GitHub Pages
+  build: {
+    outDir: "dist/projects", // Set the output directory to dist/projects
+  },
   plugins: [react()],
 });
